c4d581964313bbadc55dd10cd801000f0d1358ec
Ratske99 <sethlaske9999@gmail.com>
2023-04-22 12:55:05 -0700

    Bear Sight Mechanic

--- Jamming/Assets/Scripts/Enemy/Bear.cs
+++ Jamming/Assets/Scripts/Enemy/Bear.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Bear : MonoBehaviour
 {
-    public float movespeed;
+    public float walkspeed;
+    public float runspeed;
     public float rotatespeed;
     public Rigidbody2D rb;
     public PathPost firstpost;
@@ -12,42 +14,52 @@ public class Bear : MonoBehaviour
     public LayerMask obstacleLayer;
     public Transform seen;
     public Vector3 lastposition;
+    public NavMeshAgent agent;
+    //public Vector3 direction;
+    public bool focused;
+    public float degreestorotate = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         nextpost = firstpost.transform.position;
-        StartCoroutine(ChangeDirection());
+        agent.SetDestination(nextpost);
+        //StartCoroutine(ChangeDirection());
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (degreestorotate == 0) {
+            rotate(agent.velocity);
+        }
+        
+
+        //Debug.Log("Remaining Distance: " + agent.remainingDistance);
         if (seen != null)
         {
-            //Rotates towards object and speeds up
-            Vector3 targetvector = (seen.position - transform.position).normalized;
-            float angle = Mathf.Atan2(targetvector.y, targetvector.x) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(0, 0, angle + 90);
-            Debug.Log("Roatating");
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 2 * rotatespeed * Time.deltaTime);
-            rb.velocity = (seen.position - transform.position).normalized * 2 * movespeed;
-
-            //Checks line of sight again
+            agent.SetDestination(seen.position);
             Vector3 direction = seen.position - transform.position;
             RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, direction.magnitude, obstacleLayer);
             if (hit.collider != null)
             {
                 lastposition = seen.position;
+
+                agent.SetDestination(lastposition);
                 seen = null;
-                StartCoroutine(ChangeDirection());
+                StartCoroutine(LostSight());
             }
         }
-        else if (seen != null) { 
-            //NavMesh to seen
-            //Once arrived do 360
-            //NavMesh to next point
+        else {
+            //rotate(agent.velocity);
         }
+
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -55,8 +67,9 @@ public class Bear : MonoBehaviour
         
         if (collision.gameObject.tag == "path") {
             nextpost = collision.GetComponent<PathPost>().nextpost.transform.position;
-            if (seen == null) {
-                StartCoroutine(ChangeDirection());
+            if (focused == false) {
+                //StartCoroutine(ChangeDirection());
+                agent.SetDestination(nextpost);
             }
             
         }
@@ -64,27 +77,55 @@ public class Bear : MonoBehaviour
         else if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Player Eaten");
+            //gameover
         }
     }
 
-    IEnumerator ChangeDirection() {
-        rb.velocity = Vector2.zero;
+    /*IEnumerator ChangeDirection() {
+        //rb.velocity = Vector2.zero;
+        //rotate(nextpost - transform.position);
         Vector3 targetvector = (nextpost - transform.position).normalized;
         float angle = Mathf.Atan2(targetvector.y, targetvector.x) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(0, 0, angle + 90);
         while (transform.rotation != targetRotation)
         {
-            Debug.Log("Roatating");
+            //Debug.Log("Rotating");
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotatespeed * Time.deltaTime);
             yield return new WaitForSeconds(Time.deltaTime);
         }
         //transform.rotation = Quaternion.Euler(0, 0, angle + 90);
 
         //yield return new WaitForSeconds(1f);
-        rb.velocity = (nextpost - transform.position).normalized * movespeed;
+        //rb.velocity = (nextpost - transform.position).normalized * movespeed;
+    }*/
+
+    IEnumerator LostSight() {
+        //Approach the last remaining location
+        while (agent.remainingDistance > .2f) {
+            Debug.Log("Approaching last known location");
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        agent.ResetPath();
+        //Do a full 360
+        Quaternion initialdirection = transform.rotation;
+
+        
+        
+        while (degreestorotate < 360)
+        {
+            Debug.Log("doing a 360");
+            degreestorotate += rotatespeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rotatespeed * 1 * Time.deltaTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        degreestorotate = 0;
+        //go to the next thing
+        Debug.Log("moving on with my life");
+        focused = false;
+        agent.speed = walkspeed;
+        agent.SetDestination(nextpost);
+
     }
-
-
 
     private float normalize_angle_left_right(float angle)
     {
@@ -95,11 +136,20 @@ public class Bear : MonoBehaviour
 
     public void ApproachSight(Transform Location)
     {
+        focused = true;
         seen = Location;
+        agent.speed = runspeed;
         Debug.Log("The bear has seen something and will go straight to it");
     }
 
     public void ApproachAudio(Vector3 Location) {
         Debug.Log("The bear is bothered and will approach the noise");
+    }
+
+    public void rotate(Vector3 targetdirection) {
+        targetdirection = targetdirection.normalized;
+        float angle = Mathf.Atan2(targetdirection.y, targetdirection.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle + 90);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotatespeed * Time.deltaTime);
     }
 }
