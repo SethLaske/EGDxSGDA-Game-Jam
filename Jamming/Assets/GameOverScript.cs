f6c327d5a69c92e0df03b03e7b431deb3f75b227
Tsu <112430082+lchowGH@users.noreply.github.com>
2023-04-22 19:26:34 -0700

    Menu Update + a few more

---
+++ Jamming/Assets/GameOverScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+
+    public void gameOver()
+    {
+        // Need an if for when player touches bear
+        gameOverUI.SetActive(true);
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
