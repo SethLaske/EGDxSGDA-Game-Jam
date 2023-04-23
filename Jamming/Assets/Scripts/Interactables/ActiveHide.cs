d95284815dc1618f85e62ea6f213a875e48ea900
NotALobster <92136499+NotALobster@users.noreply.github.com>
2023-04-23 13:00:42 -0700

    hiding indicator is a thing now
    yeah

--- Jamming/Assets/Scripts/Interactables/ActiveHide.cs
+++ Jamming/Assets/Scripts/Interactables/ActiveHide.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class ActiveHide : Interactable
 {
+    private HideIndicator ind;
+    private bool h;
+    private void Start()
+    {
+        ind = GetComponentInChildren<HideIndicator>();
+        h = false;
+    }
     public override InteractType Interact(PlayerInteract playerInteract)
     {
+        h = !h;
+        ind.PlayerHide(h);
         return InteractType.ActiveHide; //tells controller what type it was (in this case, makes controller know this item was destroyed
     }
 }
--- Jamming/Assets/Scripts/Interactables/HideIndicator.cs
+++ Jamming/Assets/Scripts/Interactables/HideIndicator.cs
@@ -2,30 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HideIndicator : MonoBehaviour
+public class Indicator : MonoBehaviour
 {
     private Transform sprite;
-    [SerializeField] private SpriteRenderer spriteRend;
-
     private void Start()
     {
         sprite = transform.GetChild(0);
     }
-
-    public void PlayerHide(bool playerInside)
-    {
-        if (playerInside)
-        {
-            Debug.Log("Inside");
-            spriteRend.color = new Color(spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 1f);
-        }
-        else
-        {
-            Debug.Log("Outside");
-            spriteRend.color = new Color(spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, .5f);
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
